5eb9f4c5974a6b3543d5bd186708d8311474a1a2
rikp777 <rikp777@users.noreply.github.com>
2022-01-13 22:08:24 +0000

    deploy: c509f69ffb24d8ebfa723b51aba18752e5904267

---
+++ _nuxt/92d2da8.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{492:function(e,n,r){"use strict";r.r(n);var o=r(379);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.6.2","app-compat")}}]);
