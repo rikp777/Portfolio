297a4e60f16e2da044699f8c4d0a1fce23689146
rikp777 <rikp777@users.noreply.github.com>
2022-01-17 12:13:05 +0000

    deploy: 5d7e79ae5ff45d5121753c40f86b222931eb5667

--- _nuxt/7fec581.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{511:function(e,n,r){"use strict";r.r(n);var o=r(395);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.6.2","app-compat")}}]);
---
+++ _nuxt/static/1642421540/nl/learning/language/LearningDashboard/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/nl/learning/language/LearningDashboard", {data:[{_img:{}}],fetch:{},mutations:[]});
