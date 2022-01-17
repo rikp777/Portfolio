af55b619cb2769f53b47bd93ac0c415a1204a60f
rikp777 <rikp777@users.noreply.github.com>
2022-01-17 14:59:15 +0000

    deploy: a521283de9e3162d9c38ced7cecca0b6a859ab88

--- _nuxt/static/1642429997/learning/language/LearningDashboard/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/learning/language/LearningDashboard", {data:[{_img:{}}],fetch:{},mutations:[]});
--- _nuxt/static/1642429997/nl/uses/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/nl/uses", {data:[{_img:{}}],fetch:{},mutations:[]});
